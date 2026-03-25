89a34db436f15fa6f8e78d8c0fe4ab324fc995b9
Jerry Innocent <jinnoccent@uespoir.com>
2026-03-25 13:17:49 -0700

    ajout slideBar

--- src/layouts/SideBar.jsx
+++ src/layouts/SideBar.jsx
@@ -1,34 +1,45 @@
-// Sidebar.jsx
 import { Folder } from 'lucide-react';
-import './Sidebar.css';
+import './sideBar.css';
+import { Link } from "react-router-dom"
+
+import { LayoutDashboard } from 'lucide-react';
+import { User } from 'lucide-react';
+import { PlusIcon } from 'lucide-react';
 
 export default function SideBar() {
+    const sizeIcon = 30;
+
     return (
         <div className="sidebar">
 
             <div className="system-title">
-                <Folder />
+                <Folder size={sizeIcon} />
                 <h3>HR_SYSTEM</h3>
             </div>
 
-            <nav className="nav-menu">
+            <div className="nav-menu">
 
-                <button className="nav-btn">
-                    <span className="nav-icon">📊</span>
+                {/* Lien vers dashboard la */}
+                <Link to={"/"} className="nav-btn">
+                   <LayoutDashboard size={sizeIcon} />
                     Dashboard
-                </button>
+                </Link>
 
-                <button className="nav-btn">
-                    <span className="nav-icon">👥</span>
+                {/* Lien vers employe list la */}
+                <Link to={"/list"} className="nav-btn">
+                    <User />
                     Employes
-                </button>
+                </Link>
 
-                <button className="nav-btn">
-                    <span className="nav-icon">➕</span>
+                {/* Lien vers AddEmployes la */}
+                <Link to={"/add"} className="nav-btn">
+                    <PlusIcon />
                     addEmployes
-                </button>
+                </Link>
 
-            </nav>
+
+
+            </div>
         </div>
     );
 };
