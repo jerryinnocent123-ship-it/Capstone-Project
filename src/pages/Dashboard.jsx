567b8940a363dcc5f56c3cf916cea03213ecd962
Jerry Innocent <jinnoccent@uespoir.com>
2026-03-27 11:34:27 -0700

    Kote pou mete form nan

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,14 +4,25 @@ import { empl } from "../data/empl"
 import EmployeeList from "../components/EmployeeList"
 
 
-export default function(){
-    return(
+export default function () {
+    return (
         <>
-        <Header title={"Employes Dashboard"}/>
-        <SideBar/>
-        <div>
-        <EmployeeList profil= {empl}/>
-        </div>
+            <Header title={"Employes Dashboard"} />
+            <SideBar />
+            <div className="containers">
+
+                <div className="content">
+                    <EmployeeList profil={empl} />
+                </div>
+
+                
+                <div className="content">
+                    {/* kote form nan ap ye */}
+                </div>
+
+                
+
+            </div>
         </>
     )
 }
