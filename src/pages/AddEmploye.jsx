16862455604223c6b3e98762ae32bad6791263ed
LAINE <jonesleeschneiderlaine@gmail.com>
2026-03-27 11:02:57 -0700

    mwen push tout bagay

--- src/pages/AddEmploye.jsx
+++ src/pages/AddEmploye.jsx
@@ -1,13 +1,13 @@
 import Header from "../components/Header"
 import SideBar from "../layouts/SideBar"
-
-
+import EmployeeForm from "../components/EmployeeForm"
 
 export default function AddEmploye() {
     return (
         <>
             <Header title={"Add an Employe"}/>
             <SideBar />
+           <div className="form-section"> <EmployeeForm/>  </div> 
         </>
     )
 }
